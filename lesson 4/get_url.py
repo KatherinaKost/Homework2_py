45dcfa121ad90cfc4451ae79aa78b22ca0b54f9a
Katherina <katya_71296@mail.ru>
2025-09-21 23:45:29 +0300

    4

---
+++ lesson 4/get_url.py
@@ -0,0 +1,45 @@
+
+import requests
+
+def get_duck():
+    res = requests.get('https://random-d.uk/api/random')
+    d = res.json()
+    num = d['url'].split('.')[1].split('/')[-1]
+    url_img = d['url']
+    return url_img, num
+
+def get_fox(num):
+    images = []
+    while num:
+        res = requests.get('https://randomfox.ca/floof/')
+        d = res.json()
+        images.append(d['image'])
+        num -= 1
+    return images
+
+def get_weather(city=None):
+    url = f'http://api.openweathermap.org/data/2.5/weather'
+
+    if city:
+        params = {'q': city, 'APPID': '2a4ff86f9aaa70041ec8e82db64abf56','lang': 'ru'}
+    else:
+        params = {'q': 'Minsk', 'APPID': '2a4ff86f9aaa70041ec8e82db64abf56', 'lang': 'ru'}
+
+    answer = requests.get(url, params)
+    res = answer.json()
+
+    if answer.status_code != 200:
+        return {
+                'error': True,
+                'message': res.get('message', 'Город не найден')
+            }
+    
+    result = {
+            'city': res['name'],
+            'weather':res["weather"][0]["main"],
+            'temp':round(res['main']['temp']-273.15, 1),
+            'humidity':res['main']['humidity'],
+            'feels_like':round(res['main']['feels_like']-273.15, 1),
+            'wind':res['wind']['speed']
+    }
+    return result
